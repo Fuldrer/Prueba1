49586711e146789daef20f646e531fc1cc0d592c
Fuldrer <carlosmolina1997@hotmail.com>
2018-02-14 23:12:27 -0600

    1

---
+++ cmake-build-debug/CMakeFiles/Intento2.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Intento2.dir/main.cpp.obj"
+  "CMakeFiles/Intento2.dir/DataFile.cpp.obj"
+  "CMakeFiles/Intento2.dir/Registro_2.cpp.obj"
+  "CMakeFiles/Intento2.dir/Registro_1.cpp.obj"
+  "CMakeFiles/Intento2.dir/Registro_3.cpp.obj"
+  "Intento2.pdb"
+  "Intento2.exe"
+  "Intento2.exe.manifest"
+  "libIntento2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Intento2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
